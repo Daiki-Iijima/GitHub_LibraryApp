659f43c5a8a982d967696467789aac6e5f8dd84b
Daiki-Iijima <daiki.applecreate@gmail.com>
2019-06-30 00:40:59 +0900

    first commit

---
+++ GitHub_LibraryApp/GitHub_LibraryApp/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using GitHub_LibraryApp.Models;
+
+namespace GitHub_LibraryApp.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
